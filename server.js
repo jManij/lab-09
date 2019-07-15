01ed3c7757a1a4e44b6145583ec7d3b863408e1a
Manish KC <kcmanish89@gmail.com>
2019-07-14 18:42:35 -0700

    Assignment Completed

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const pg = require('pg');
 
 //Global vars
 const PORT = process.env.PORT || 3001;
-const ROUTE_IDs = ['LOCATION', 'WEATHER', 'EVENTS', 'MOVIES', 'YELP'];
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', error => {
@@ -26,7 +25,7 @@ app.use(cors());
 app.get('/location', searchToLatLng);
 app.get('/weather', searchWeather);
 app.get('/events', searchEvents);
-// app.get('/movies', searchMovies);
+app.get('/movies', searchMovies);
 // app.get('/yelp', searchFromYelp);
 app.use('*', (req, res) => {
   res.send('You got in the wrong place')
@@ -51,7 +50,7 @@ function FormattedEvent(data) {
   this.name = data.name.text;
   this.link = data.url;
   this.event_date = new Date(data.start.local).toDateString();
-  this.summary = data.description.text;
+  this.summary = (data.description.text).slice(0, 500) + '....';
 }
 
 function FormattedMovies(data) {
@@ -71,56 +70,53 @@ function searchToLatLng(request, response) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationName}&key=${process.env.GEOCODE_API_KEY}`;
 
   getLocation(locationName, response, url); //Get location
-
 }
-
 
 function searchWeather(request, response) {
   let locationName = request.query.data.search_query;
-  let location_id = request.query.data.id;
-  console.log(request.query.data);
-  console.log('search weather location id: ', location_id);
   let lat = request.query.data.latitude;
   let long = request.query.data.longitude;
   let weatherLocation = `${lat},${long}` || '37.8267,-122.4233';
   const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${weatherLocation}`;
 
-  checkDB('location_id', location_id, 'weather', url, response)
-    .then(data => {
-      response.send(data);
-    })
-
+  start_process(locationName, 'weather', url, response);
 }
 
 function searchEvents(request, response) {
+  const locationName = request.query.data.search_query;
   let lat = request.query.data.latitude;
   let long = request.query.data.longitude;
-  const url = `https://www.eventbriteapi.com/v3/events/search/?token=${process.env.EVENTBRITE_API_KEY}&location.latitude=${lat}&location.longitude=${long}`
-  superagent.get(url)
-    .then(result => {
-      let arrayOfFormattedEvents = result.body.events.map(item => {
-        return new FormattedEvent(item);
-      })
+  const url = `https://www.eventbriteapi.com/v3/events/search/?token=${process.env.EVENTBRITE_API_KEY}&location.latitude=${lat}&location.longitude=${long}`;
 
-      response.send(arrayOfFormattedEvents);
-
-    }).catch(e => {
-      console.log(e);
-      response.status(500).send('Status 500');
-    })
+  start_process(locationName, 'events', url, response);
 }
 
 function searchMovies(request, response) {
   const movieName = request.query.data.search_query;
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query=${movieName}`
-  getSuperAgent(url, response, ROUTE_IDs.MOVIES);
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query=${movieName}`;
+
+  start_process(movieName, 'movies', url, response);
+}
+/******************---ROUTES--------**************************/
+
+
+function getLocationID(locationName) {
+  return client.query(`SELECT id FROM locations WHERE search_query=$1`, [locationName]);
 }
 
+function start_process(primaryKey, searchFor, url, response) {
+  console.log('Starting process');
+  getLocationID(primaryKey)
+    .then(item => {
+      checkDB('location_id', item.rows[0].id, searchFor, url)
+        .then(data => {
+          response.send(data);
+        })
+    })
 
-//Returns false or sends from here
-function checkDB(search_query, search_value, tableName, url, routeID, response) {
-  console.log('Check DB: ', search_value);
+}
 
+function checkDB(search_query, search_value, tableName, url) {
   return client.query(`SELECT * FROM ${tableName} WHERE ${search_query}=$1`, [search_value])
     .then(sqlResult => {
       if (sqlResult.rowCount === 0) {
@@ -143,56 +139,90 @@ const SQL_INSERTS = {
   weather: `INSERT INTO weather(
     forecast,
     time,
-    location_id) VALUES($1, $2, $3)`
+    location_id) VALUES($1, $2, $3)
+                 RETURNING *`,
+  movies: `INSERT INTO movies(
+    title,
+    overview,
+    average_votes,
+    total_votes,
+    image_url,
+    popularity,
+    released_on,
+    location_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+                  RETURNING *`,
+  events: `INSERT INTO events (
+    name,
+    link,
+    event_date,
+    summary, 
+    location_id) VALUES($1, $2, $3, $4, $5)
+                  RETURNING *`
 }
 
 function sendFromDB(sqlResult) {
   console.log('returning from DB');
-  console.log(sqlResult.rows);
   return sqlResult.rows;
 }
 
 function makeApiCALL(tableName, search_value, url) {
-  console.log('Make api call');
+  console.log('Making api call for fresh ', tableName);
+  const listOfMovies = [];
 
   switch (tableName) {
   case 'weather':
     return superagent.get(url)
       .then(result => {
-      // console.log(result.body);
+        // console.log(result.body);
         let forecastArr = result.body.daily.data.map(el => {
           return new FormattedDailyWeather(el);
         })
-        console.log(forecastArr);
+        // Store in database
         forecastArr.forEach(item => {
           return client.query(
             SQL_INSERTS[tableName], [item.forecast, item.time, search_value]
-          ).then(sqlResult => {
-            return sqlResult.rows;
-          })
-
+          )
         })
+        //send the response
+        return forecastArr;
 
       })
 
-    // case 'movies':
-    //   return superagent.get(url) 
-    //     .then(result => {
+  case 'movies':
+    return superagent.get(url)
+      .then(result => {
+        result.body.results.forEach(movie => {
+          return listOfMovies.push(new FormattedMovies(movie));
+        })
+        //Store in database
+        listOfMovies.forEach(item => {
+          client.query(
+            SQL_INSERTS[tableName], [item.title, item.overview, item.average_votes, item.total_votes, item.url, item.popularity, item.released_on, search_value]
+          )
+        })
+        //send the response
+        return listOfMovies;
+      })
 
-    //     })
+  case 'events':
+    return superagent.get(url)
+      .then(result => {
+        let arrayOfFormattedEvents = result.body.events.map(item => {
+          return new FormattedEvent(item);
+        })
+        //Store in database
+        arrayOfFormattedEvents.forEach(event => {
+          client.query(
+            SQL_INSERTS[tableName], [event.name, event.link, event.event_date, event.summary, search_value]
+          )
+        })
+        //Send the response
+        return arrayOfFormattedEvents;
+      })
 
   }//switch ends
 
-
 }
-
-
-
-
-
-
-/******************---ROUTES--------**************************/
-
 
 /******************---HELPER FUNCTIONS--------**************************/
 
